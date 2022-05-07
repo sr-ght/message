454163e2408ce2b9428d5c57794a57b709ca6c9b
Thiago Rodrigues <sr.ght@protonmail.ch>
2022-05-07 13:43:03 -0300

    Splash and Welcome

--- app/src/main/java/com/tibe/message/view/activity/WelcomeActivity.java
+++ app/src/main/java/com/tibe/message/view/activity/WelcomeActivity.java
@@ -1,19 +1,23 @@
 package com.tibe.message.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.tibe.message.R;
+import com.tibe.message.databinding.ActivityWelcomeBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
+
+    ActivityWelcomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.Theme_Message);
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
 
         try {
             Thread.sleep(2000);
